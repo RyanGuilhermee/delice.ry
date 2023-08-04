7f121902a9acd272363461bd5647ccb0ae708211
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-08-04 12:51:00 -0300

    feat: format methods returns and controller endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,61 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('users')
+@Controller('api/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
+  async create(@Body() createUserDto: CreateUserDto) {
+    const user = await this.usersService.create(createUserDto);
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
+    return {
+      statusCode: 201,
+      user: {
+        ...user,
+      },
+    };
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    const user = await this.usersService.findOne(id);
+
+    return {
+      statusCode: 200,
+      user: {
+        ...user,
+      },
+    };
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    const response = await this.usersService.update(id, updateUserDto);
+
+    return {
+      statusCode: 200,
+      message: response,
+    };
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  async remove(@Param('id') id: string) {
+    const response = await this.usersService.remove(id);
+
+    return {
+      statusCode: 200,
+      message: response,
+    };
   }
 }
