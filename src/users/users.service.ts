75f1dc7985df54e97880f0fb31a7e5061194fc4e
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-08-04 12:41:02 -0300

    feat: implement users repository in users service and add validations

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  IUsersRepository,
+  UsersRepository,
+} from '../repositories/users.repository';
+import bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+export class UsersService implements IUsersRepository {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.findOneByEmail(createUserDto.email);
+
+    if (user) {
+      throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+    }
+
+    const hashPassword = await bcrypt.hash(createUserDto.password, 10);
+    createUserDto.password = hashPassword;
+
+    return this.usersRepository.create(createUserDto);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findOne(id: string) {
+    return this.usersRepository.findOne(id);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOneByEmail(email: string) {
+    return this.usersRepository.findOneByEmail(email);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      const hashPassword = await bcrypt.hash(updateUserDto.password, 10);
+      updateUserDto.password = hashPassword;
+    }
+
+    return this.usersRepository.update(id, updateUserDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return this.usersRepository.remove(id);
   }
 }
