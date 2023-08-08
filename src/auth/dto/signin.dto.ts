838a87be1876c18beb017dfd7e7419b081107a99
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-08-08 14:20:32 -0300

    docs: add properties mapping to signin dto

--- src/auth/dto/signin.dto.ts
+++ src/auth/dto/signin.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsEmail, MinLength } from 'class-validator';
 
 export class SignInDto {
+  @ApiProperty({ type: 'string' })
   @IsString()
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ type: 'string' })
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
