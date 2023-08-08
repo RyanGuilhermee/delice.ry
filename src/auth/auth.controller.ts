9e03035d74242421cbb465353dff9b87898c6ea0
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-08-08 16:06:59 -0300

    docs: add response type in swagger to auth controller methods

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,40 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin.dto';
+import { ApiResponse } from '@nestjs/swagger';
+import { SignInAuthResponse } from './response_types/signin-auth.response';
 
 @Controller('api/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiResponse({ status: 200, type: SignInAuthResponse })
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  signIn(@Body() signInDto: SignInDto) {
-    return this.authService.signIn(signInDto.email, signInDto.password);
+  async signIn(@Body() signInDto: SignInDto) {
+    const token = await this.authService.signIn(
+      signInDto.email,
+      signInDto.password,
+    );
+
+    return {
+      statusCode: 200,
+      accessToken: token,
+    };
   }
 
+  @ApiResponse({ status: 200, type: SignInAuthResponse })
   @HttpCode(HttpStatus.OK)
   @Post('login/admin')
-  signInAdmin(@Body() signInDto: SignInDto) {
-    return this.authService.signInAdmin(signInDto.email, signInDto.password);
+  async signInAdmin(@Body() signInDto: SignInDto) {
+    const token = await this.authService.signInAdmin(
+      signInDto.email,
+      signInDto.password,
+    );
+
+    return {
+      statusCode: 200,
+      accessToken: token,
+    };
   }
 }
