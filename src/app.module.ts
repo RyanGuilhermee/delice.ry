d8c33216d27f08508c96dd93799f8b68a85653a7
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-08-09 00:31:08 -0300

    feat: add menu resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { MenuModule } from './menu/menu.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, AuthModule],
+  imports: [ConfigModule.forRoot(), UsersModule, AuthModule, MenuModule],
   controllers: [],
   providers: [],
 })
