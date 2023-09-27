1db2d07f8d82cb234557605add3f82c0a9d0a235
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-09-27 20:24:24 -0300

    feat: add orders controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,34 +1,85 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { FindAllOrderDto } from './dto/findall-order.dto';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
 
-@Controller('orders')
+@Controller('api/orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @Roles('admin', 'everyone')
+  @UseGuards(AuthGuard)
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto);
+  async create(@Body() createOrderDto: CreateOrderDto) {
+    const order = await this.ordersService.create(createOrderDto);
+
+    return {
+      statusCode: 201,
+      order,
+    };
   }
 
+  @Roles('admin', 'everyone')
+  @UseGuards(AuthGuard)
   @Get()
-  findAll() {
-    return this.ordersService.findAll();
+  async findAll(@Query() findAllOrderDto: FindAllOrderDto) {
+    const orders = await this.ordersService.findAll(findAllOrderDto);
+
+    return {
+      statusCode: 200,
+      orders,
+    };
   }
 
+  @Roles('admin', 'everyone')
+  @UseGuards(AuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ordersService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    const order = await this.ordersService.findOne(id);
+
+    return {
+      statusCode: 200,
+      ...order,
+    };
   }
 
+  @Roles('admin')
+  @UseGuards(AuthGuard)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.ordersService.update(+id, updateOrderDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateOrderDto: UpdateOrderDto,
+  ) {
+    const response = await this.ordersService.update(id, updateOrderDto);
+
+    return {
+      statusCode: 200,
+      message: response,
+    };
   }
 
+  @Roles('admin', 'everyone')
+  @UseGuards(AuthGuard)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ordersService.remove(+id);
+  async remove(@Param('id') id: string) {
+    const response = await this.ordersService.remove(id);
+
+    return {
+      statusCode: 200,
+      message: response,
+    };
   }
 }
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { OrdersRepository } from '../repositories/orders.repostory';
+import { UsersService } from '../users/users.service';
+import { UsersRepository } from '../repositories/users.repository';
+import { MenuService } from '../menu/menu.service';
+import { MenuRepository } from '../repositories/menu.repository';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [
+    OrdersService,
+    OrdersRepository,
+    UsersService,
+    UsersRepository,
+    MenuService,
+    MenuRepository,
+  ],
 })
 export class OrdersModule {}
