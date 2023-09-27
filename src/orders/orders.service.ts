55da087e436a65a5f3fd33eff306301fe5041fc0
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-09-27 20:23:40 -0300

    feat: add orders service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,26 +1,79 @@
-import { Injectable } from '@nestjs/common';
-import { CreateOrderDto } from './dto/create-order.dto';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { CreateOrderDto, PaymentType } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import {
+  IOrdersRepository,
+  OrdersRepository,
+} from '../repositories/orders.repostory';
+import { FindAllOrderDto } from './dto/findall-order.dto';
+import { UsersService } from '../users/users.service';
+import { MenuService } from '../menu/menu.service';
 
 @Injectable()
-export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+export class OrdersService implements IOrdersRepository {
+  constructor(
+    private ordersRepository: OrdersRepository,
+    private usersService: UsersService,
+    private menuService: MenuService,
+  ) {}
+
+  async create(createOrderDto: CreateOrderDto) {
+    const user = await this.usersService.findOne(createOrderDto.userId);
+    const menu = await this.menuService.findOne(createOrderDto.menuId);
+
+    if (!user) {
+      throw new BadRequestException('Invalid user id');
+    }
+
+    if (!menu) {
+      throw new BadRequestException('Invalid menu id');
+    }
+
+    if (
+      createOrderDto.paymentType !== PaymentType.MONEY &&
+      !createOrderDto.isPaid
+    ) {
+      throw new BadRequestException('Payment needs to be made to continue');
+    }
+
+    return this.ordersRepository.create(createOrderDto);
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findAll(findAllOrderDto: FindAllOrderDto) {
+    if (!findAllOrderDto.userId) {
+      throw new ForbiddenException('Inform your user id to get this resource');
+    }
+
+    if (!findAllOrderDto.quantity || isNaN(Number(findAllOrderDto.quantity))) {
+      findAllOrderDto.quantity = '10';
+    }
+
+    if (!findAllOrderDto.page || isNaN(Number(findAllOrderDto.page))) {
+      findAllOrderDto.page = '1';
+    }
+
+    const user = await this.usersService.findOne(findAllOrderDto.userId);
+
+    if (user.isAdmin) {
+      findAllOrderDto.userId = '';
+    }
+
+    return this.ordersRepository.findAll(findAllOrderDto);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  findOne(id: string) {
+    return this.ordersRepository.findOne(id);
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
+  update(id: string, updateOrderDto: UpdateOrderDto) {
+    return this.ordersRepository.update(id, updateOrderDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+  remove(id: string) {
+    return this.ordersRepository.remove(id);
   }
 }
