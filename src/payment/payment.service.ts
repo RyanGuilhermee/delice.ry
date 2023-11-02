edb394033892eb7a2364a2f6eb459f3428c61679
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-11-02 15:52:53 -0300

    feat: implements payment repository

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,35 +1,19 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreatePaymentDto } from './dto/create-payment.dto';
-import { MercadoPagoConfig, Payment } from 'mercadopago';
+import {
+  IPaymentRepository,
+  PaymentRepository,
+} from '../repositories/payment.repository';
 
 @Injectable()
-export class PaymentService {
-  private client = new MercadoPagoConfig({
-    accessToken: process.env.PAYMENT_ACCESS_TOKEN,
-  });
-  private payment = new Payment(this.client);
+export class PaymentService implements IPaymentRepository {
+  constructor(private paymentRepository: PaymentRepository) {}
 
-  async create(createPaymentDto: CreatePaymentDto) {
-    try {
-      const paymentResponse = await this.payment.create({
-        body: {
-          ...createPaymentDto,
-        },
-      });
-
-      return paymentResponse;
-    } catch (error) {
-      throw new BadRequestException(error);
-    }
+  createPayment(createPaymentDto: CreatePaymentDto) {
+    return this.paymentRepository.createPayment(createPaymentDto);
   }
 
-  async findOne(id: number) {
-    try {
-      const payment = await this.payment.get({ id: id.toString() });
-
-      return payment;
-    } catch (error) {
-      throw new BadRequestException(error);
-    }
+  findPayment(id: number) {
+    return this.paymentRepository.findPayment(id);
   }
 }
