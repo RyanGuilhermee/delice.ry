d069f896a7de0669ed4e490577b400ccfa549b61
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-11-02 15:54:37 -0300

    feat: add payment repository as a provider

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { PaymentRepository } from '../repositories/payment.repository';
 
 @Module({
   controllers: [PaymentController],
-  providers: [PaymentService],
+  providers: [PaymentService, PaymentRepository],
 })
 export class PaymentModule {}
