ed1347718ba1e61b2d2e0cacedef6c835eb9bf06
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-11-02 15:55:01 -0300

    test: add payment service tests

--- src/payment/payment.service.spec.ts
+++ src/payment/payment.service.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentService } from './payment.service';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { CreatePaymentDto } from './dto/create-payment.dto';
+import { PaymentRepository } from '../repositories/payment.repository';
 
 describe('PaymentService', () => {
   let service: PaymentService;
+  let paymentRepository: DeepMocked<PaymentRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [PaymentService],
-    }).compile();
+    })
+      .useMocker(createMock)
+      .compile();
 
     service = module.get<PaymentService>(PaymentService);
+    paymentRepository = module.get(PaymentRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should successfully create a payment', async () => {
+      paymentRepository.createPayment.mockResolvedValueOnce({
+        status: 'approved',
+        api_response: { headers: ['', []], status: 200 },
+      });
+
+      await expect(
+        service.createPayment(new CreatePaymentDto()),
+      ).resolves.toEqual({
+        status: 'approved',
+        api_response: { headers: ['', []], status: 200 },
+      });
+    });
   });
 });
