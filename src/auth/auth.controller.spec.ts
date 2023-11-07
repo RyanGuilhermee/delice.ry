761095867f2dce1b9f5606129987fbe6d5868cb0
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-11-07 14:14:09 -0300

    test: add unit tests to auth service and controller

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,20 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
+import { SignInDto } from './dto/signin.dto';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: DeepMocked<AuthService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
-    }).compile();
+    })
+      .useMocker(createMock)
+      .compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('signIn', () => {
+    it('should successfully sign in an user', async () => {
+      authService.signIn.mockResolvedValue('jwt-token');
+
+      await expect(controller.signIn(new SignInDto())).resolves.toEqual({
+        statusCode: 200,
+        accessToken: 'jwt-token',
+      });
+    });
+  });
+
+  describe('signInAdmin', () => {
+    it('should successfully sign in an admin', async () => {
+      authService.signInAdmin.mockResolvedValue('jwt-token');
+
+      await expect(controller.signInAdmin(new SignInDto())).resolves.toEqual({
+        statusCode: 200,
+        accessToken: 'jwt-token',
+      });
+    });
   });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,171 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let usersService: DeepMocked<UsersService>;
+  let jwtService: DeepMocked<JwtService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService],
-    }).compile();
+    })
+      .useMocker(createMock)
+      .compile();
 
     service = module.get<AuthService>(AuthService);
+    usersService = module.get(UsersService);
+    jwtService = module.get(JwtService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('signIn', () => {
+    it('should successfully sign in an user', async () => {
+      usersService.findOneByEmail.mockResolvedValueOnce({
+        id: '123',
+        name: 'test',
+        email: 'test@email',
+        password: 'sjndajsn18h73y29377fhgd',
+        is_admin: false,
+      });
+
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockImplementation(() => Promise.resolve(true));
+
+      jwtService.signAsync.mockResolvedValueOnce('jwt-token');
+
+      await expect(service.signIn('test@email', '12345678')).resolves.toBe(
+        'jwt-token',
+      );
+    });
+
+    it('should throw an exception if user email is wrong', async () => {
+      usersService.findOneByEmail.mockResolvedValueOnce(null);
+
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockImplementation(() => Promise.resolve(false));
+
+      await expect(
+        service.signIn('test2@email', '12345678'),
+      ).rejects.toBeInstanceOf(UnauthorizedException);
+
+      await expect(service.signIn('test2@email', '12345678')).rejects.toThrow(
+        'User email or pessword wrong',
+      );
+    });
+
+    it('should throw an exception if user password is wrong', async () => {
+      usersService.findOneByEmail.mockResolvedValueOnce({
+        id: '123',
+        name: 'test',
+        email: 'test@email',
+        password: 'sjndajsn18h73y29377fhgd',
+        is_admin: false,
+      });
+
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockImplementation(() => Promise.resolve(false));
+
+      await expect(
+        service.signIn('test@email', '123456789'),
+      ).rejects.toBeInstanceOf(UnauthorizedException);
+
+      await expect(service.signIn('test@email', '123456789')).rejects.toThrow(
+        'User email or pessword wrong',
+      );
+    });
+  });
+
+  describe('signInAdmin', () => {
+    it('should successfully sign in an admin', async () => {
+      usersService.findOneByEmail.mockResolvedValueOnce({
+        id: '123',
+        name: 'test',
+        email: 'test@email',
+        password: 'sjndajsn18h73y29377fhgd',
+        is_admin: true,
+      });
+
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockImplementation(() => Promise.resolve(true));
+
+      jwtService.signAsync.mockResolvedValueOnce('jwt-token');
+
+      await expect(service.signInAdmin('test@email', '12345678')).resolves.toBe(
+        'jwt-token',
+      );
+    });
+
+    it('should throw an exception if admin email is wrong', async () => {
+      usersService.findOneByEmail.mockResolvedValueOnce(null);
+
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockImplementation(() => Promise.resolve(false));
+
+      await expect(
+        service.signInAdmin('test2@email', '12345678'),
+      ).rejects.toBeInstanceOf(UnauthorizedException);
+
+      await expect(
+        service.signInAdmin('test2@email', '12345678'),
+      ).rejects.toThrow('User email or pessword wrong');
+    });
+
+    it('should throw an exception if admin password is wrong', async () => {
+      usersService.findOneByEmail.mockResolvedValueOnce({
+        id: '123',
+        name: 'test',
+        email: 'test@email',
+        password: 'sjndajsn18h73y29377fhgd',
+        is_admin: true,
+      });
+
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockImplementation(() => Promise.resolve(false));
+
+      await expect(
+        service.signInAdmin('test@email', '123456789'),
+      ).rejects.toBeInstanceOf(UnauthorizedException);
+
+      await expect(
+        service.signInAdmin('test@email', '123456789'),
+      ).rejects.toThrow('User email or pessword wrong');
+    });
+
+    it('should throw an exception if user is not admin', async () => {
+      usersService.findOneByEmail.mockResolvedValue({
+        id: '123',
+        name: 'test',
+        email: 'test@email',
+        password: 'sjndajsn18h73y29377fhgd',
+        is_admin: false,
+      });
+
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockImplementation(() => Promise.resolve(true));
+
+      await expect(
+        service.signInAdmin('test@email', '12345678'),
+      ).rejects.toBeInstanceOf(UnauthorizedException);
+
+      await expect(
+        service.signInAdmin('test@email', '12345678'),
+      ).rejects.toThrow('User is not admin');
+    });
   });
 });
