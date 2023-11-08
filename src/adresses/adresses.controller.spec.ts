05946c30676122535cca429270f005d01bf9e834
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-11-08 14:35:56 -0300

    test: add unit tests to adresses service and controller

--- src/adresses/adresses.controller.spec.ts
+++ src/adresses/adresses.controller.spec.ts
@@ -1,20 +1,100 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdressesController } from './adresses.controller';
 import { AdressesService } from './adresses.service';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { CreateAdressDto } from './dto/create-adress.dto';
+import { FindAdressDto } from './dto/find-adress.dto';
+import { UpdateAdressDto } from './dto/update-adress.dto';
 
 describe('AdressesController', () => {
   let controller: AdressesController;
+  let adressesService: DeepMocked<AdressesService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AdressesController],
-      providers: [AdressesService],
-    }).compile();
+    })
+      .useMocker(createMock)
+      .compile();
 
     controller = module.get<AdressesController>(AdressesController);
+    adressesService = module.get(AdressesService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should successfully create an adress', async () => {
+      const adress = {
+        id: '123',
+        street: 'some street',
+        number: 10,
+      } as FindAdressDto;
+
+      adressesService.create.mockResolvedValueOnce(adress);
+
+      await expect(controller.create(new CreateAdressDto())).resolves.toEqual({
+        statusCode: 201,
+        adress,
+      });
+    });
+  });
+
+  describe('findAll', () => {
+    it('should successfully find all adresses', async () => {
+      adressesService.findAll.mockResolvedValueOnce([]);
+
+      await expect(controller.findAll('123')).resolves.toEqual({
+        statusCode: 200,
+        adresses: [],
+      });
+    });
+  });
+
+  describe('findOne', () => {
+    it('should successfully find an adress', async () => {
+      const adress = {
+        id: '123',
+        street: 'some street',
+        number: 10,
+      } as FindAdressDto;
+
+      adressesService.findOne.mockResolvedValueOnce(adress);
+
+      await expect(controller.findOne('123')).resolves.toEqual({
+        statusCode: 200,
+        adress,
+      });
+    });
+  });
+
+  describe('update', () => {
+    it('should successfully update an adress', async () => {
+      adressesService.update.mockResolvedValueOnce(
+        'Adress updated successfully!',
+      );
+
+      await expect(
+        controller.update('123', new UpdateAdressDto()),
+      ).resolves.toEqual({
+        statusCode: 200,
+        message: 'Adress updated successfully!',
+      });
+    });
+  });
+
+  describe('remove', () => {
+    it('should successfully remove an adress', async () => {
+      adressesService.remove.mockResolvedValueOnce(
+        'Adress removed successfully!',
+      );
+
+      await expect(controller.remove('123')).resolves.toEqual({
+        statusCode: 200,
+        message: 'Adress removed successfully!',
+      });
+    });
   });
 });
--- src/adresses/adresses.service.spec.ts
+++ src/adresses/adresses.service.spec.ts
@@ -1,18 +1,101 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdressesService } from './adresses.service';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { AdressesRepository } from '../repositories/adresses.repository';
+import { CreateAdressDto } from './dto/create-adress.dto';
+import { UpdateAdressDto } from './dto/update-adress.dto';
+import { NotFoundException } from '@nestjs/common';
 
 describe('AdressesService', () => {
   let service: AdressesService;
+  let adressesRepository: DeepMocked<AdressesRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AdressesService],
-    }).compile();
+    })
+      .useMocker(createMock)
+      .compile();
 
     service = module.get<AdressesService>(AdressesService);
+    adressesRepository = module.get(AdressesRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should successfully create an adress', async () => {
+      adressesRepository.create.mockResolvedValueOnce({
+        id: '123',
+        street: 'some street',
+        number: 10,
+      });
+
+      await expect(service.create(new CreateAdressDto())).resolves.toEqual({
+        id: '123',
+        street: 'some street',
+        number: 10,
+      });
+    });
+  });
+
+  describe('findAll', () => {
+    it('should successfully find all adresses', async () => {
+      adressesRepository.findAll.mockResolvedValueOnce([]);
+
+      await expect(service.findAll('123')).resolves.toEqual([]);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should successfully find an adress', async () => {
+      adressesRepository.findOne.mockResolvedValueOnce({
+        id: '123',
+        street: 'some street',
+        number: 10,
+      });
+
+      await expect(service.findOne('123')).resolves.toEqual({
+        id: '123',
+        street: 'some street',
+        number: 10,
+      });
+    });
+
+    it('should throw an exception if an adress was not found', async () => {
+      adressesRepository.findOne.mockResolvedValue(null);
+
+      await expect(service.findOne('1')).rejects.toBeInstanceOf(
+        NotFoundException,
+      );
+
+      await expect(service.findOne('1')).rejects.toThrow('Adress not found');
+    });
+  });
+
+  describe('update', () => {
+    it('should successfully update an adress', async () => {
+      adressesRepository.update.mockResolvedValueOnce(
+        'Adress updated successfully!',
+      );
+
+      await expect(service.update('123', new UpdateAdressDto())).resolves.toBe(
+        'Adress updated successfully!',
+      );
+    });
+  });
+
+  describe('remove', () => {
+    it('should successfully remove an adress', async () => {
+      adressesRepository.remove.mockResolvedValueOnce(
+        'Adress removed successfully!',
+      );
+
+      await expect(service.remove('123')).resolves.toBe(
+        'Adress removed successfully!',
+      );
+    });
   });
 });
